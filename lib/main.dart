6210b1fd254005c107691fe4e325e1cac2929b1e
Angel Gonzalez <leonardo2002376@gmail.com>
2023-10-25 12:50:21 -0700

    Se agrego el ChangeNotifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:english_words/english_words.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,30 +12,35 @@ class MyApp extends StatelessWidget {
  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(),
       ),
-      home: const MyHomePage(),
     );
   }
 }
 
+class MyAppState extends ChangeNotifier{
+  var current = WordPair.random();
+}
+
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
-
- 
-
   @override
   Widget build(BuildContext context){
+    var appState = context.watch<MyAppState>();
     return Scaffold(
       body: Column(
         children: [
         Text("Idea aleatoria"),
-        Text("Aqui va la idea"),
+        Text(appState.current.asLowerCase),
         ],
       ),
     );
