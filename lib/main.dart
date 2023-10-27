4ec83b5027ea2d12f2d14949896c3b2862f0ec6b
Angel Gonzalez <leonardo2002376@gmail.com>
2023-10-27 12:43:50 -0700

    Agregamos boton me gusta

--- lib/main.dart
+++ lib/main.dart
@@ -29,18 +29,36 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier{
   var current = WordPair.random();
+  var favoritos = <WordPair>[];
 
   void getSiguiente(){
     current = WordPair.random();
     notifyListeners();
   }
+void toggleFavoritos(){
+  if (favoritos.contains(current)){
+    favoritos.remove(current);   
+}else{
+  favoritos.add(current);
 }
+notifyListeners();
+
+}
+}
+
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
   @override
   Widget build(BuildContext context){
     var appState = context.watch<MyAppState>();
+    var idea= appState.current;
+    IconData icon;
+    if (appState.favoritos.contains(idea)){
+      icon = Icons.favorite;
+    }else{
+      icon = Icons.favorite_outline;
+    }
     return Scaffold(
       body: Center(
         child: Column(
@@ -48,11 +66,21 @@ class MyHomePage extends StatelessWidget {
           children: [
           Bigcard(idea: (appState.current)),
           SizedBox(height: 10,),
-          ElevatedButton(
-            onPressed: (){
-              appState.getSiguiente();
-            }, 
-            child: Text("Siguiente"))
+          Row(
+            mainAxisSize:MainAxisSize.min ,
+            children: [
+              ElevatedButton.icon(
+                onPressed: (){appState.toggleFavoritos();},
+                icon: Icon(icon),
+                label: Text("Me gusta")),
+              SizedBox(width: 10,),
+              ElevatedButton(
+                onPressed: (){
+                  appState.getSiguiente();
+                }, 
+                child: Text("Siguiente")),
+            ],
+          )
           ],
         ),
       ),
