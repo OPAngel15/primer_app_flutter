3a2b35cec9e67e7e57380a59735cc4c14ac6045a
Angel Gonzalez <leonardo2002376@gmail.com>
2023-11-01 11:29:28 -0700

    Se agrego favoritoscomo placefolder

--- lib/main.dart
+++ lib/main.dart
@@ -58,7 +58,13 @@ class _MyHomePageState extends State<MyHomePage> {
   var selectedIndex = 0;
   @override
   Widget build(BuildContext context){
-
+    Widget page;
+    switch(selectedIndex) {
+      case 0: page = GeneratorPage(); break;
+      case 1: page = Placeholder(); break;
+      default: 
+      throw UnimplementedError('No hay un widget para: $selectedIndex');
+    }
     return Scaffold(
       body: Row(
         children: [
@@ -79,13 +85,14 @@ class _MyHomePageState extends State<MyHomePage> {
               });
             },
           ),
+          ),
           Expanded(
             child: Container(
               color: Theme.of(context).colorScheme.primaryContainer,
-              child: GeneratorPage(),
-            )),
-        ],)
-    );
+              child: page,)),
+         ], ),
+        )
+    ;
   }
 }
 
